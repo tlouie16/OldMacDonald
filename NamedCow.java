e38fff28f7d7730dc44d163e220cc1e3d132ac65
trlouie1 <trlouie1@s.sfusd.edu>
2017-10-20 10:41:54 -0700

    repush

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,15 @@
-class NamedCow
-{
-
+class NamedCow extends Cow
+{    
+ protected String myName;
+     public NamedCow(String type, String name, String sound)     {         
+         myType = type;     
+         mySound = sound;    
+         myName= name;
+     }     
+     public NamedCow()     {           
+         myName = "unknown";  
+     }      
+     public String getName(){
+       return myName;
+     }
 }
